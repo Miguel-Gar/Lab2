47e4d0b73dd77f4d57e17c987a87cff12370d09f
hp <hp@LAPTOP-49AMUBG8>
2022-02-07 09:07:59 -0600

    Agregar archivos de proyecto.

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonbuscar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonbuscar
+            // 
+            this.buttonbuscar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonbuscar.Location = new System.Drawing.Point(414, 88);
+            this.buttonbuscar.Name = "buttonbuscar";
+            this.buttonbuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonbuscar.TabIndex = 0;
+            this.buttonbuscar.Text = "Buscar";
+            this.buttonbuscar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonbuscar);
+            this.Name = "Form1";
+            this.Text = "Lab2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonbuscar;
+    }
+}
+
