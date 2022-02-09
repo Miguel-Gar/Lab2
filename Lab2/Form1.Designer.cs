b21eb0b0c017f6f1926deb5139fbc09c408862bb
Miguel-Gar <nickegarcy1@gmail.com>
2022-02-08 18:04:03 -0600

    ya encuentra y ve a los lins con https:// entonces sol faltara que busque sin el https://

--- Lab2/Form1.Designer.cs
+++ Lab2/Form1.Designer.cs
@@ -29,33 +29,120 @@
         private void InitializeComponent()
         {
             this.buttonbuscar = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaAtrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haciaAdelanteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonbuscar
             // 
             this.buttonbuscar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonbuscar.Location = new System.Drawing.Point(414, 88);
+            this.buttonbuscar.Location = new System.Drawing.Point(288, 27);
             this.buttonbuscar.Name = "buttonbuscar";
             this.buttonbuscar.Size = new System.Drawing.Size(75, 23);
             this.buttonbuscar.TabIndex = 0;
-            this.buttonbuscar.Text = "Buscar";
+            this.buttonbuscar.Text = "Ir";
             this.buttonbuscar.UseVisualStyleBackColor = true;
+            this.buttonbuscar.Click += new System.EventHandler(this.buttonbuscar_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "https://calendar.google.com/calendar/u/0/r",
+            "https://www.mesoamericana.edu.gt/"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(270, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 54);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(768, 320);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.haciaAtrasToolStripMenuItem,
+            this.haciaAdelanteToolStripMenuItem,
+            this.iToolStripMenuItem});
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.inicioToolStripMenuItem.Text = "Navegador";
+            this.inicioToolStripMenuItem.Click += new System.EventHandler(this.inicioToolStripMenuItem_Click);
+            // 
+            // haciaAtrasToolStripMenuItem
+            // 
+            this.haciaAtrasToolStripMenuItem.Name = "haciaAtrasToolStripMenuItem";
+            this.haciaAtrasToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.haciaAtrasToolStripMenuItem.Text = "Inicio";
+            this.haciaAtrasToolStripMenuItem.Click += new System.EventHandler(this.haciaAtrasToolStripMenuItem_Click);
+            // 
+            // haciaAdelanteToolStripMenuItem
+            // 
+            this.haciaAdelanteToolStripMenuItem.Name = "haciaAdelanteToolStripMenuItem";
+            this.haciaAdelanteToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.haciaAdelanteToolStripMenuItem.Text = "Hacia Atras";
+            this.haciaAdelanteToolStripMenuItem.Click += new System.EventHandler(this.haciaAdelanteToolStripMenuItem_Click);
+            // 
+            // iToolStripMenuItem
+            // 
+            this.iToolStripMenuItem.Name = "iToolStripMenuItem";
+            this.iToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.iToolStripMenuItem.Text = "Hacia Adelante";
+            this.iToolStripMenuItem.Click += new System.EventHandler(this.iToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 407);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonbuscar);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Lab2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button buttonbuscar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaAtrasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haciaAdelanteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iToolStripMenuItem;
     }
 }
 
--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -16,5 +16,71 @@ namespace Lab2
         {
             InitializeComponent();
         }
+
+        private void buttonbuscar_Click(object sender, EventArgs e)
+        {
+            string nombre = "";
+            string nombre2 = "";
+            comboBox1.Text =nombre;
+            webBrowser1.Navigate(new Uri(comboBox1.SelectedItem.ToString()));
+            for (int x = 1; x < nombre.Length; x++)
+            {
+
+                //si la posición anterior a la letra es un espacio en blanco
+                //convertirla a mayúscula
+                //sino solo copiarla
+                if (nombre[x - 1] == ' ')
+                {
+                    if (char.IsLetter(nombre[x]) || char.IsWhiteSpace(nombre[x]))
+                        nombre2 += char.ToUpper(nombre[x]);
+                }
+                else
+                  if (char.IsLetter(nombre[x]) || char.IsWhiteSpace(nombre[x]))
+                    nombre2 += nombre[x];
+
+            }
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //comboBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void haciaAtrasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoHome();
+        }
+
+        private void iToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void haciaAdelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            webBrowser1.GoHome();
+        }
     }
 }
